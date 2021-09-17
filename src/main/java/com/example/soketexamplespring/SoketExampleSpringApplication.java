04f050bd891980e7cded8254802600998cfdaa97
77kkyu <77kkyu@gmail.com>
2021-09-17 11:16:45 +0900

    bulid - 프로젝트 초기 설정

---
+++ src/main/java/com/example/soketexamplespring/SoketExampleSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.soketexamplespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoketExampleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoketExampleSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/soketexamplespring/SoketExampleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soketexamplespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoketExampleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
